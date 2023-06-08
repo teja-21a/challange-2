0582a0332bf6f875cfb07947d78ba55d025c321d
tej <pavanteja123ll@gmail.com>
2023-06-08 23:50:30 +0530

    addition_division_feature_merge_

--- trapizoid.py
+++ trapizoid.py
@@ -1,9 +1,6 @@
 number1 = int(input("enter the number"))
 number2 = int(input("enter the number"))
 height = int(input("enter the number"))
-<<<<<<< HEAD
 sum = number1 + number2
-=======
 
 half_sum = sum/2 
->>>>>>> feature/division
